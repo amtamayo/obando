9f14284d9adc5c43442bf40c4e1b3709b97f86b1
amarie <amtamayo@gmail.com>
2012-05-05 23:24:58 -0500

    setup to run using rackup

--- lib/obando.rb
+++ lib/obando.rb
@@ -1,7 +1,1 @@
-#myapp.rb
-require 'rubygems'
-require 'sinatra'
-
-get '/' do
-  send_file File.join(settings.public_folder, 'index.html')
-end
+require "obando/application"
---
+++ lib/obando/application.rb
@@ -0,0 +1,28 @@
+require "sinatra/base"
+require "sinatra/reloader"
+require "sinatra-initializers"
+require "sinatra/r18n"
+
+module Obando
+ class Application < Sinatra::Base
+   enable :logging, :sessions
+   enable :dump_errors, :show_exceptions if development?
+
+   configure :development do
+    register Sinatra::Reloader
+   end
+
+   before do
+    session[:locale] = params[:locale] if params[:locale]
+   end
+
+   use Rack::Logger
+   use Rack::Session::Cookie
+
+
+   get '/' do
+    send_file File.expand_path('index.html', settings.public_folder)
+   end
+
+ end
+end
